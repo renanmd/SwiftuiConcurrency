5fc80221e95e055d5e7cac7d997dfe584c24a2ff
Renan Muller <renanmd@gmail.com>
2024-02-12 17:55:16 -0300

    Initial Commit

---
+++ SwiftuiConcurrency/SwiftuiConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiConcurrencyApp.swift
+//  SwiftuiConcurrency
+//
+//  Created by Renan Diniz on 12/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
